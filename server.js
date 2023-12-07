d9385d067f867854577d63cf9982fe4616c7367d
easylearningbd <elebdsoft@gmail.com>
2023-12-08 04:35:06 +0600

    p19 Project Backend Setup Part 3

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+app.use(cors({
+    origin : ['http://localhost:3000'],
+    credentials : true
+}))
+app.use(bodyParser.json())
+app.use(cookieParser())
 
 
 app.use('/api',require('./routes/authRoutes'))
