f945369233e930e70efc0995b976e77414813028
easylearningbd <elebdsoft@gmail.com>
2023-12-09 02:29:20 +0600

    p20 Admin Login Page Setup Part 1

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,14 @@
 class authControllers{
     admin_login = async(req,res) => {
-        console.log(req.body)
+        const {email,password} = req.body
+        try {
+            
+        } catch (error) {
+            
+        }
+
+
+
     }
 }
 
