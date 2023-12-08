6131ea0ed9f4c163960c4016db3123f3800882d4
easylearningbd <elebdsoft@gmail.com>
2023-12-09 04:17:45 +0600

    p22 Admin Login Page Setup Part 3

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,7 @@
 const adminModel = require('../models/adminModel')
 const { responseReture } = require('../utiles/response')
+const bcrpty = require('bcrypt')
+const { createToken } = require('../utiles/tokenCreate')
 
 class authControllers{
     admin_login = async(req,res) => {
@@ -8,6 +10,23 @@ class authControllers{
             const admin = await adminModel.findOne({email}).select('+password')
             // console.log(admin)
             if (admin) {
+                const match = await bcrpty.compare(password, admin.password)
+                // console.log(match)
+                if (match) {
+                    const token = await createToken({
+                        id : admin.id,
+                        role : admin.role
+                    })
+                    res.cookie('accessToken',token,{
+                        expires : new Date(Date.now() + 7*24*60*60*1000 )
+                    })
+                    responseReture(res,200,{token,message: "Login Success"})
+                } else {
+                    responseReture(res,404,{error: "Password Wrong"})
+                }
+
+
+
                  
             } else {
                 responseReture(res,404,{error: "Email not Found"})
