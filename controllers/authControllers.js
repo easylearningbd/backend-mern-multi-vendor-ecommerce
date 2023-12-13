6dc1fc0055b4e3e66e4c6639d4ecbf4e4fc465fb
easylearningbd <elebdsoft@gmail.com>
2023-12-14 01:09:02 +0600

    p28 Middleware And Routes Manage Part 2

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -35,10 +35,31 @@ class authControllers{
         } catch (error) {
             responseReturn(res,500,{error: error.message})
         }
-
-
-
+ 
     }
+    // End Method 
+
+    getUser = async (req, res) => {
+        const {id, role} = req;
+
+        try {
+            if (role === 'admin') {
+                const user = await adminModel.findById(id)
+                responseReturn(res, 200, {userInfo : user})
+            }else {
+                console.log('Seller Info')
+            }
+            
+        } catch (error) {
+            console.log(error.message)
+        }
+
+
+    } // End getUser Method 
+
+
+
+
 }
 
 module.exports = new authControllers()
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,8 @@
 const authControllers = require('../controllers/authControllers')
+const { authMiddleware } = require('../middlewares/authMiddleware')
 const router = require('express').Router()
 
 router.post('/admin-login',authControllers.admin_login)
+router.get('/get-user',authMiddleware, authControllers.getUser)
 
 module.exports = router
