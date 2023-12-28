23bbace080c4e01f8e395711d4e0c30e279dc299
easylearningbd <elebdsoft@gmail.com>
2023-12-29 05:21:59 +0600

    p100 Seller Register and  Login Part 4

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,6 @@
 const adminModel = require('../models/adminModel')
 const sellerModel = require('../models/sellerModel')
+const sellerCustomerModel  = require('../models/chat/sellerCustomerModel')
 const { responseReturn } = require('../utiles/response')
 const bcrpty = require('bcrypt')
 const { createToken } = require('../utiles/tokenCreate')
@@ -55,12 +56,22 @@ class authControllers{
                     method : 'menualy',
                     shopInfo: {}
                 })
-                console.log(seller)
+               await sellerCustomerModel.create({
+                     myId: seller.id
+               })
+
+               const token = await createToken({ id : seller.id, role: seller.role })
+               res.cookie('accessToken',token, {
+                expires : new Date(Date.now() + 7*24*60*60*1000 )
+               })
+
+               responseReturn(res,201,{token,message: 'Register Success'})
             }
          } catch (error) {
-            console.log(error)
+            responseReturn(res,500,{error: 'Internal Server Error'})
          }
     }
+    // End Method 
 
 
 
