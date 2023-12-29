36dc72bd80e1bdb35cb030f4250c18d836df4d5a
easylearningbd <elebdsoft@gmail.com>
2023-12-30 01:49:13 +0600

    p102 Seller Register and  Login Part 6

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,5 +5,6 @@ const router = require('express').Router()
 router.post('/admin-login',authControllers.admin_login)
 router.get('/get-user',authMiddleware, authControllers.getUser)
 router.post('/seller-register',authControllers.seller_register)
+router.post('/seller-login',authControllers.seller_login)
 
 module.exports = router
