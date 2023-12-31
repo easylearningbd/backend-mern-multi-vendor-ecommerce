15e3c872a812f34fd83e7d4d4b070c087ef4da40
easylearningbd <elebdsoft@gmail.com>
2024-01-01 04:25:39 +0600

    p114 Setup Category Add And Get Part 4

--- controllers/dasboard/categoryController.js
+++ controllers/dasboard/categoryController.js
@@ -1,6 +1,25 @@
+const formidable = require("formidable")
+const { responseReturn } = require("../../utiles/response")
+
 class categoryController{
 
     add_category = async (req, res) => {
+        const form = formidable()
+        form.parse(req, async(err,fields,files)=>{
+            if (err) {
+                responseReturn(res, 404,{ error : 'something went wrong'})
+            } else {
+                let {name} = fields
+                let {image} = files
+                name = name.trim()
+                const slug = name.split(' ').join('-')
+            }
+             
+        })
+    }
+
+
+    get_category = async (req, res) => {
         console.log('this is working')
     }
 
--- routes/dashboard/categoryRoutes.js
+++ routes/dashboard/categoryRoutes.js
@@ -3,5 +3,6 @@ const { authMiddleware } = require('../../middlewares/authMiddleware')
 const router = require('express').Router()
 
 router.post('/category-add',authMiddleware, categoryController.add_category) 
+router.get('/category-get',authMiddleware, categoryController.get_category) 
 
 module.exports = router
