0f1c22aee5abbf14d978b7c3270618ff89d2cad4
easylearningbd <elebdsoft@gmail.com>
2024-01-05 17:44:07 +0600

    p124 Add Product Setup  Part 5

---
+++ models/productModel.js
@@ -0,0 +1,70 @@
+const {Schema, model} = require("mongoose");
+
+const productSchema = new Schema({
+    sellerId: {
+        type: Schema.ObjectId,
+        required : true
+    },
+    name: {
+        type: String,
+        required : true
+    },
+    slug: {
+        type: String,
+        required : true
+    },
+    category: {
+        type: String,
+        required : true
+    },
+    brand: {
+        type: String,
+        required : true
+    },
+    price: {
+        type: Number,
+        required : true
+    },
+    stock: {
+        type: Number,
+        required : true
+    },
+    discount: {
+        type: Number,
+        required : true
+    },
+    description: {
+        type: String,
+        required : true
+    },
+    shopName: {
+        type: String,
+        required : true
+    },
+    images: {
+        type: Array,
+        required : true
+    },
+    rating: {
+        type: Number,
+        default : 0
+    } 
+     
+}, {timestamps: true})
+
+productSchema.index({
+    name: 'text',
+    category: 'text',
+    brand: 'text',
+    description: 'text'
+},{
+    weights: {
+        name: 5,
+        category: 4,
+        brand: 3,
+        description: 2
+    }
+
+})
+
+module.exports = model('products',productSchema)
