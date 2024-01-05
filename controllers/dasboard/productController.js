d04d3a289b4f34c50514efbf2cda23fcf197265b
easylearningbd <elebdsoft@gmail.com>
2024-01-06 05:03:45 +0600

    p128 Product Show Part 1

--- controllers/dasboard/productController.js
+++ controllers/dasboard/productController.js
@@ -54,6 +54,38 @@ class productController{
 
     /// end method 
 
+    products_get = async (req, res) => {
+        const {page,searchValue, parPage} = req.query 
+        const {id} = req;
+
+       const skipPage = parseInt(parPage) * (parseInt(page) - 1)
+
+        try {
+
+            if (searchValue) {
+                const products = await productModel.find({
+                    $text: { $search: searchValue },
+                    sellerId: id
+                }).skip(skipPage).limit(parPage).sort({ createdAt: -1})
+                const totalProduct = await productModel.find({
+                    $text: { $search: searchValue },
+                    sellerId: id
+                }).countDocuments()
+                responseReturn(res, 200,{products,totalProduct})
+            } else {
+                const products = await productModel.find({ sellerId:id }).skip(skipPage).limit(parPage).sort({ createdAt: -1})
+            const totalProduct = await productModel.find({ sellerId:id }).countDocuments()
+            responseReturn(res, 200,{products,totalProduct}) 
+            }
+            
+        } catch (error) {
+            
+        } 
+
+    }
+
+    // End Method 
+
 
 
 
--- routes/dashboard/productRoutes.js
+++ routes/dashboard/productRoutes.js
@@ -3,5 +3,6 @@ const { authMiddleware } = require('../../middlewares/authMiddleware')
 const router = require('express').Router()
 
 router.post('/product-add',authMiddleware, productController.add_product)  
+router.get('/products-get',authMiddleware, productController.products_get)  
 
 module.exports = router
