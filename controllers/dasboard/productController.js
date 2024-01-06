a933157d67ef9454ec33be8b2c9594c82c5ad740
easylearningbd <elebdsoft@gmail.com>
2024-01-07 03:21:11 +0600

    p132 Product Edit and Update Part 2

--- controllers/dasboard/productController.js
+++ controllers/dasboard/productController.js
@@ -79,11 +79,22 @@ class productController{
             }
             
         } catch (error) {
-            
+            console.log(error.message)
         } 
 
     }
 
+    // End Method 
+
+    product_get = async (req, res) => {
+        const { productId } = req.params;
+        try {
+            const product = await productModel.findById(productId)
+            responseReturn(res, 200,{product})
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
     // End Method 
 
 
--- routes/dashboard/productRoutes.js
+++ routes/dashboard/productRoutes.js
@@ -4,5 +4,6 @@ const router = require('express').Router()
 
 router.post('/product-add',authMiddleware, productController.add_product)  
 router.get('/products-get',authMiddleware, productController.products_get)  
+router.get('/product-get/:productId',authMiddleware, productController.product_get)  
 
 module.exports = router
