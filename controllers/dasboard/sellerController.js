f4621254c516ecca84cca9c1006586859c48375e
easylearningbd <elebdsoft@gmail.com>
2024-01-08 05:31:40 +0600

    p145 Seller Request Show Add Part 3

--- controllers/dasboard/sellerController.js
+++ controllers/dasboard/sellerController.js
@@ -1,12 +1,26 @@
 const formidable = require("formidable")
 const { responseReturn } = require("../../utiles/response")
 const cloudinary = require('cloudinary').v2
-const categoryModel = require('../../models/categoryModel')
+const sellerModel = require('../../models/sellerModel')
 
 class sellerController{ 
 
     request_seller_get = async (req, res) => {
-        console.log(req.query)
+        const {page,searchValue, parPage} = req.query 
+        const skipPage = parseInt(parPage) * (parseInt(page) - 1)
+
+        try {
+            if (searchValue) {
+                
+            } else {
+                const sellers = await sellerModel.find({ status:  'pending'}).skip(skipPage).limit(parPage).sort({ createdAt: -1})
+                const totalSeller = await sellerModel.find({ status: 'pending' }).countDocuments()
+                responseReturn(res, 200,{ sellers,totalSeller })
+            }
+        } catch (error) {
+            responseReturn(res, 500,{ error: error.message }) 
+        }
+ 
     }
 
     
