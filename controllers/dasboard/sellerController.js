ff61bbc2c01720e627ec20e9200325f481d1e39c
easylearningbd <elebdsoft@gmail.com>
2024-01-08 17:00:23 +0600

    p148 Sellar Details And Status Manage Part 3

--- controllers/dasboard/sellerController.js
+++ controllers/dasboard/sellerController.js
@@ -38,6 +38,19 @@ class sellerController{
 
      // end method 
 
+     seller_status_update = async (req, res) => {
+        const {sellerId, status} = req.body
+        try {
+            await sellerModel.findByIdAndUpdate(sellerId,{status})
+            const seller = await sellerModel.findById(sellerId)
+            responseReturn(res, 200,{ seller,  message: 'Seller Status Updated Successfully' })
+        } catch (error) {
+            responseReturn(res, 500,{ error: error.message })
+        }
+    }
+
+     // end method 
+
 
 
 
--- routes/dashboard/sellerRoutes.js
+++ routes/dashboard/sellerRoutes.js
@@ -4,4 +4,5 @@ const router = require('express').Router()
 
 router.get('/request-seller-get',authMiddleware, sellerController.request_seller_get)  
 router.get('/get-seller/:sellerId',authMiddleware, sellerController.get_seller)  
+router.post('/seller-status-update',authMiddleware, sellerController.seller_status_update)  
 module.exports = router
