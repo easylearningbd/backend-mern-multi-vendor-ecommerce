ecf4b3ae3fd841d7214f9daa320226583ae8ebf9
easylearningbd <elebdsoft@gmail.com>
2024-01-28 04:23:03 +0600

    p209 Fetch Feature Product and Latest Top Rated Discount Part 2

--- controllers/home/homeControllers.js
+++ controllers/home/homeControllers.js
@@ -4,8 +4,22 @@ const { responseReturn } = require("../../utiles/response")
 
 class homeControllers{
 
-    formateProduct = () => {
-        
+    formateProduct = (products) => {
+        const productArray = [];
+        let i = 0;
+        while (i < products.length ) {
+            let temp = []
+            let j = i
+            while (j < i + 3) {
+                if (products[j]) {
+                    temp.push(products[j])
+                }
+                j++
+            }
+            productArray.push([...temp])
+            i = j
+        }
+        return productArray
     }
 
     get_categorys = async(req,res) => {
