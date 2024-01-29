305ff89f4f0d6625547f7c0d9581f468c0e5dc46
easylearningbd <elebdsoft@gmail.com>
2024-01-30 01:59:50 +0600

    p228 Customer Register Setup Part 3

--- controllers/home/customerAuthController.js
+++ controllers/home/customerAuthController.js
@@ -1,8 +1,35 @@
+const customerModel = require('../../models/customerModel')
+const { responseReturn } = require('../../utiles/response')
+const bcrypt = require('bcrypt')
+const sellerCustomerModel = require('../../models/chat/sellerCustomerModel')
+
 class customerAuthController{
 
     customer_register = async(req,res) => {
-        console.log(req.body)
+        const {name, email, password } = req.body
+
+        try {
+            const customer = await customerModel.findOne({email}) 
+            if (customer) {
+                responseReturn(res, 404,{error : 'Email Already Exits'} )
+            } else {
+                const createCustomer = await customerModel.create({
+                    name: name.trim(),
+                    email: email.trim(),
+                    password: await bcrypt.hash(password, 10),
+                    method: 'menualy'
+                })
+                await sellerCustomerModel.create({
+                    myId: createCustomer.id
+                })
+                
+            }
+        } catch (error) {
+            
+        }
     }
+    // End Method
+
 
 }
 
