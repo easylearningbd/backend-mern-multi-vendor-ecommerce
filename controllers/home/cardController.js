6a3fc3e47962e3d3b949faf8f6aa13ef94eb4b2c
easylearningbd <elebdsoft@gmail.com>
2024-01-30 18:10:17 +0600

    p237 Product Add to Card Part 3

--- controllers/home/cardController.js
+++ controllers/home/cardController.js
@@ -1,7 +1,39 @@
+const cardModel = require('../../models/cardModel')
+const { responseReturn } = require('../../utiles/response')
+
 class cardController{
    
     add_to_card =  async(req, res) => {
-        console.log(req.body)
+        const { userId, productId, quantity } = req.body
+        try {
+            const product = await cardModel.findOne({
+                $and: [{
+                    productId : {
+                        $eq: productId
+                    }
+                },
+                {
+                    userId: {
+                        $eq: userId
+                    }
+                }
+            ]
+            })
+
+            if (product) {
+                responseReturn(res,404,{error: "Product Already Added To Card" })
+            } else {
+                const product = await cardModel.create({
+                    userId,
+                    productId,
+                    quantity
+                })
+                responseReturn(res,201,{message: "Added To Card Successfully" , product})
+            }
+            
+        } catch (error) {
+            console.log(error.message)
+        }
     }
     // End Method 
 }
