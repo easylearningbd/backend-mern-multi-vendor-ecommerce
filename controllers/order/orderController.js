0a5fa904bea4bcd4ea81bdf2ca939a6a9bb19ed2
easylearningbd <elebdsoft@gmail.com>
2024-02-01 02:35:17 +0600

    p255 Customer Order Place Part 4

--- controllers/order/orderController.js
+++ controllers/order/orderController.js
@@ -1,6 +1,16 @@
+const authOrderModel = require('../../models/authOrder')
+const customerOrder = require('../../models/customerOrder')
+const moment = require("moment")
+
+ 
+
 class orderController{
     place_order = async (req,res) => {
-        console.log(req.body)
+        const {price,products,shipping_fee,shippingInfo,userId } = req.body
+        let authorOrderData = []
+        let cardId = []
+        const tempDate = moment(Date.now()).format('LLL')
+        console.log(tempDate)
     }
 
     // End Method 
---
+++ models/authOrder.js
@@ -0,0 +1,38 @@
+const {Schema, model} = require("mongoose");
+
+const authSchema = new Schema({
+    orderId: {
+        type: Schema.ObjectId,
+        required : true
+    },
+    sellerId: {
+        type: Schema.ObjectId,
+        required : true
+    },
+    products: {
+        type: Array,
+        required : true  
+    }, 
+    price: {
+        type: Number,
+        required : true  
+    },     
+    payment_status: {
+        type: String,
+        required : true  
+    },
+    shippingInfo: {
+        type: Object,
+        required : true  
+    },
+    delivery_status: {
+        type: String,
+        required : true  
+    },
+    date: {
+        type: String,
+        required : true
+    } 
+},{ timestamps: true })
+
+module.exports = model('authorOrders',authSchema)
