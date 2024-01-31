4069fca9ce77e8bdc9bc08ed5a448c8fc861ba26
easylearningbd <elebdsoft@gmail.com>
2024-02-01 03:34:16 +0600

    p256 Customer Order Place Part 5

--- controllers/order/orderController.js
+++ controllers/order/orderController.js
@@ -10,7 +10,27 @@ class orderController{
         let authorOrderData = []
         let cardId = []
         const tempDate = moment(Date.now()).format('LLL')
-        console.log(tempDate)
+
+        let customerOrderProduct = []
+
+        for (let i = 0; i < products.length; i++) {
+            const pro = products[i].products
+            for (let j = 0; j < pro.length; j++) {
+                const tempCusPro = pro[j].productInfo;
+                tempCusPro.quantity = pro[j].quantity
+                customerOrderProduct.push(tempCusPro)
+                if (pro[j]._id) {
+                    cardId.push(pro[j]._id)
+                } 
+            } 
+        }
+
+        try {
+            
+        } catch (error) {
+            
+        }
+ 
     }
 
     // End Method 
