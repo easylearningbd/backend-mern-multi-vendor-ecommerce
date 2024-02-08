ec04546b2c97245029644b1673fb0e170658f5f1
easylearningbd <elebdsoft@gmail.com>
2024-02-08 20:20:18 +0600

    p275 Customer Orders Handle Part 2

--- controllers/order/orderController.js
+++ controllers/order/orderController.js
@@ -131,7 +131,28 @@ class orderController{
      // End Method 
 
      get_orders = async (req, res) => {
-        console.log(req.params)
+        const {customerId, status} = req.params
+
+        try {
+            let orders = []
+            if (status !== 'all') {
+                orders = await customerOrder.find({
+                    customerId: new ObjectId(customerId),
+                    delivery_status: status
+                })
+            } else {
+                orders = await customerOrder.find({
+                    customerId: new ObjectId(customerId)
+                })
+            }
+            responseReturn(res, 200,{
+                orders
+            })
+            
+        } catch (error) {
+            console.log(error.message)
+        }
+
      }
  // End Method 
 
