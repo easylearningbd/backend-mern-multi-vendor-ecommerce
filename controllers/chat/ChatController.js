3978e03d1b1ee1d58bdcc7141fa709833ccf40fb
easylearningbd <elebdsoft@gmail.com>
2024-02-13 17:32:45 +0600

    p307 Customer to Seller Chat System Part 6

--- controllers/chat/ChatController.js
+++ controllers/chat/ChatController.js
@@ -1,13 +1,87 @@
+const sellerModel = require('../../models/sellerModel')
+const customerModel = require('../../models/customerModel')
+const sellerCustomerModel = require('../../models/chat/sellerCustomerModel')
 class ChatController{
 
     add_customer_friend = async (req, res) => {
-        console.log(req.body)
+         const { sellerId, userId} = req.body
+
+         try {
+            if (sellerId !== '') {
+                const seller = await sellerModel.findById(sellerId)
+                const user = await customerModel.findById(userId)
+                const checkSeller = await sellerCustomerModel.findOne({
+                    $and : [
+                        {
+                            myId: {
+                                $eq: userId
+                            }
+                        },{
+                            myFriends : {
+                                $elemMatch : {
+                                    fdId : sellerId
+                                }
+                            } 
+                        }
+                    ]
+                })
+               if (!checkSeller) {
+                  await sellerCustomerModel.updateOne({
+                      myId: userId
+                  }, {
+                    $push: {
+                        myFriends: {
+                            fdId : sellerId,
+                            name: seller.shopInfo?.shopName,
+                            image: seller.image
+                        }
+                    }
+                  })
+               }
+
+
+               const checkCustomer = await sellerCustomerModel.findOne({
+                $and : [
+                    {
+                        myId: {
+                            $eq: sellerId
+                        }
+                    },{
+                        myFriends : {
+                            $elemMatch : {
+                                fdId : userId
+                            }
+                        } 
+                    }
+                ]
+            })
+           if (!checkCustomer) {
+              await sellerCustomerModel.updateOne({
+                  myId: sellerId
+              }, {
+                $push: {
+                    myFriends: {
+                        fdId : userId,
+                        name: user.name,
+                        image: ""
+                    }
+                }
+              })
+           }
+
+
+
+            }
+            
+         } catch (error) {
+            console.log(error)
+         }
     }
     // End Method 
 
 
 
-    
+
 }
 
 
