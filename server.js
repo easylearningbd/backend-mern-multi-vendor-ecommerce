475398cb55ac4ef095036c42695120be3f3be719
easylearningbd <elebdsoft@gmail.com>
2024-02-14 01:27:17 +0600

    p313 Seller to Customer Chat Complete Part 1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const socket = require('socket.io')
 const http = require('http')
 const server = http.createServer(app)
 app.use(cors({
-    origin : ['http://localhost:3000'],
+    origin : ['http://localhost:3000','http://localhost:3001'],
     credentials: true
 }))
 
