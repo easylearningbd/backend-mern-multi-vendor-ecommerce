34acad5629cb3445a4ad0f4b2251c7c033a8cf7b
easylearningbd <elebdsoft@gmail.com>
2024-02-13 20:40:57 +0600

    p311 Customer to Seller Chat System Part 10

--- controllers/chat/ChatController.js
+++ controllers/chat/ChatController.js
@@ -120,7 +120,60 @@ class ChatController{
     // End Method 
 
     customer_message_add = async (req, res) => {
-        console.log(req.body)
+        const {userId,text,sellerId,name } = req.body
+
+        try {
+            const message = await sellerCustomerMessage.create({
+                senderId: userId,
+                senderName: name,
+                receverId: sellerId,
+                message : text 
+            })
+
+            const data = await sellerCustomerModel.findOne({ myId : userId })
+            let myFriends = data.myFriends
+            let index = myFriends.findIndex(f => f.fdId === sellerId)
+            while (index > 0) {
+                let temp = myFriends[index]
+                myFriends[index] = myFriends[index - 1]
+                myFriends[index - 1] = temp
+                index--
+            }
+            await sellerCustomerModel.updateOne(
+                {
+                    myId: userId
+                },
+                {
+                    myFriends
+                }
+
+            )
+
+
+
+            const data1 = await sellerCustomerModel.findOne({ myId : sellerId })
+            let myFriends1 = data1.myFriends
+            let index1 = myFriends1.findIndex(f => f.fdId === userId)
+            while (index1 > 0) {
+                let temp1 = myFriends1[index1]
+                myFriends1[index1] = myFriends[index1 - 1]
+                myFriends1[index1 - 1] = temp1
+                index1--
+            }
+            await sellerCustomerModel.updateOne(
+                {
+                    myId: sellerId
+                },
+                {
+                    myFriends1
+                } 
+            )
+
+            responseReturn(res, 201,{message})
+
+        } catch (error) {
+            console.log(error)
+        }
     }
   // End Method 
 
