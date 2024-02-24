6b7a2fe9848a94f190b53555f03193f87eeae6f3
easylearningbd <elebdsoft@gmail.com>
2024-02-25 00:27:52 +0600

    p360 Seller Stripe Connect Account Create Part 3

--- controllers/payment/paymentController.js
+++ controllers/payment/paymentController.js
@@ -1,3 +1,7 @@
+const stripeModel = require('../../models/stripeModel')
+const {v4: uuidv4} = require('uuid')
+const stripe = require('stripe')('sk_test_51Oml5cGAwoXiNtjJZbPFBKav0pyrR8GSwzUaLHLhInsyeCa4HI8kKf2IcNeUXc8jc8XVzBJyqjKnDLX9MlRjohrL003UDGPZgQ')
+
 class paymentController{
 
     create_stripe_connect_account = async(req,res) => {
