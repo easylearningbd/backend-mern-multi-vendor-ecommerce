9bacb342debc9aff6b14e0fa0f337e84c244e749
easylearningbd <elebdsoft@gmail.com>
2024-02-27 04:18:15 +0600

    p388 Dashboard Index Data Show Part 2

--- controllers/dasboard/dashboardController.js
+++ controllers/dasboard/dashboardController.js
@@ -1,9 +1,43 @@
+const { responseReturn } = require("../../utiles/response") 
+const myShopWallet = require('../../models/myShopWallet')
+const productModel = require('../../models/productModel')
+const customerOrder = require('../../models/customerOrder')
+const sellerModel = require('../../models/sellerModel') 
+const adminSellerMessage = require('../../models/chat/adminSellerMessage') 
+
 class dashboardController{
 
 
     get_admin_dashboard_data = async(req, res) => {
         const {id} = req 
-        console.log(id)
+        try {
+            const totalSale = await myShopWallet.aggregate([
+                {
+                    $group: {
+                        _id:null,
+                        totalAmount: {$sum: '$amount'}
+                    }
+                }
+            ])
+         const totalProduct = await productModel.find({}).countDocuments()
+         const totalOrder = await customerOrder.find({}).countDocuments()
+         const totalSeller = await sellerModel.find({}).countDocuments()
+         const messages = await adminSellerMessage.find({}).limit(3)
+         const recentOrders = await customerOrder.find({}).limit(5)
+         responseReturn(res, 200, {
+            totalProduct,
+            totalOrder,
+            totalSeller,
+            messages,
+            recentOrders,
+            totalSale: totalSale.length > 0 ? totalSale[0].totalAmount : 0,
+
+         })
+
+        } catch (error) {
+            console.log(error.message)
+        }
+         
     }
     //end Method 
 
